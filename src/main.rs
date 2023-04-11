25be50566d10791518e63c9528d25a70188ea621
박정현 <pjh9996@naver.com>
2023-04-12 08:04:40 +0900

    feat: 그랩 스킬 구현

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,22 @@ struct GGEZ {
     foreground_image: Image,
     target_image: Image,
     player_image: Image,
+    player_grab_image: Image,
+    grab_string_image: Image,
+    grab_hand_image: Image,
     player_x: f32,
     player_target_x: f32,
     player_target_direction: i32,
+    player_grab_time: f32,
+    player_grab_max_time: f32,
+    player_grab_distance: f32,
+    player_grab_target_radian: f32,
+    player_grab_string_position: Point2<f32>,
     opponent_x: f32,
     player_state: i32,
     player_speed: f32,
     target_speed: f32,
+    grab_speed: f32,
 }
 
 impl EventHandler for GGEZ {
@@ -33,18 +42,26 @@ impl EventHandler for GGEZ {
         if self.player_x < 300.0 { self.player_x = 300.0; }
         if self.player_x > 980.0 { self.player_x = 980.0; }
 
-        if self.player_target_direction == -1 {
-            self.player_target_x -= self.target_speed * dt;
-            if self.player_target_x < 200.0 {
-                self.player_target_x = 200.0;
-                self.player_target_direction = 1;
+        if self.player_state != 2 {
+            if self.player_target_direction == -1 {
+                self.player_target_x -= self.target_speed * dt;
+                if self.player_target_x < 200.0 {
+                    self.player_target_x = 200.0;
+                    self.player_target_direction = 1;
+                }
+            }
+            if self.player_target_direction == 1 {
+                self.player_target_x += self.target_speed * dt;
+                if self.player_target_x > 1080.0 {
+                    self.player_target_x = 1080.0;
+                    self.player_target_direction *= -1;
+                }
             }
         }
-        if self.player_target_direction == 1 {
-            self.player_target_x += self.target_speed * dt;
-            if self.player_target_x > 1080.0 {
-                self.player_target_x = 1080.0;
-                self.player_target_direction *= -1;
+        else {
+            self.player_grab_time += dt;
+            if self.player_grab_time >= self.player_grab_max_time {
+                self.player_state = 0;
             }
         }
 
@@ -56,19 +73,50 @@ impl EventHandler for GGEZ {
         draw(ctx, &self.background_image, DrawParam::new())?;
         draw(ctx, &self.foreground_image, DrawParam::new())?;
 
-        let player_target_radian = (580.0_f64).atan2((self.player_x - self.player_target_x) as f64) as f32;
-        let player_target_draw_params = DrawParam::new()
-            .dest(Point2 {x: self.player_x, y: 650.0})
-            .rotation(player_target_radian + PI / 2.0)
-            .offset(Point2 {x: 0.5, y: 0.0 })
-            .color(Color::CYAN);
-        draw(ctx, &self.target_image, player_target_draw_params)?;
+        if self.player_state == 2 {
+            let player_target_draw_params = DrawParam::new()
+                .dest(Point2 {x: self.player_x, y: 650.0})
+                .rotation(self.player_grab_target_radian)
+                .offset(Point2 {x: 0.5, y: 0.0 })
+                .color(Color::BLACK);
+            draw(ctx, &self.target_image, player_target_draw_params)?;
 
-        let player_draw_params = DrawParam::new()
-            .dest(Point2 {x: self.player_x, y: 650.0})
-            .rotation(if self.player_state == -1 { 135.0 * PI / 180.0 } else { if self.player_state == 1 { 225.0 * PI / 180.0 } else { 180.0 * PI / 180.0 }})
-            .offset(Point2 {x: 0.5, y: 0.5 });
-        draw(ctx, &self.player_image, player_draw_params)?;
+            let player_draw_params = DrawParam::new()
+                .dest(Point2 {x: self.player_x, y: 650.0})
+                .rotation(self.player_grab_target_radian)
+                .offset(Point2 {x: 0.5, y: 0.5 });
+            draw(ctx, &self.player_grab_image, player_draw_params)?;
+
+            let grab_string_draw_params = DrawParam::new()
+                .dest(self.player_grab_string_position)
+                .rotation(self.player_grab_target_radian)
+                .offset(Point2 {x: 0.5, y: 0.0 })
+                .scale([1.0, self.player_grab_distance / self.grab_string_image.height() as f32 * self.player_grab_time / self.player_grab_max_time]);
+            draw(ctx, &self.grab_string_image, grab_string_draw_params)?;
+
+            let grab_straigt_radian = self.player_grab_target_radian + 270.0 * PI / 180.0;
+            let player_grab_hand_position = Point2 {x: self.player_grab_string_position.x - grab_straigt_radian.cos() * self.player_grab_time * self.grab_speed, y: self.player_grab_string_position.y - grab_straigt_radian.sin() * self.player_grab_time * self.grab_speed};
+            let grab_hand_draw_params = DrawParam::new()
+                .dest(player_grab_hand_position)
+                .rotation(self.player_grab_target_radian)
+                .offset(Point2 {x: 0.5, y: 0.0 });
+            draw(ctx, &self.grab_hand_image, grab_hand_draw_params)?;
+        }
+        else {
+            let player_target_radian = (580.0_f64).atan2((self.player_x - self.player_target_x) as f64) as f32 + PI / 2.0;
+            let player_target_draw_params = DrawParam::new()
+                .dest(Point2 {x: self.player_x, y: 650.0})
+                .rotation(player_target_radian)
+                .offset(Point2 {x: 0.5, y: 0.0 })
+                .color(Color::CYAN);
+            draw(ctx, &self.target_image, player_target_draw_params)?;
+
+            let player_draw_params = DrawParam::new()
+                .dest(Point2 {x: self.player_x, y: 650.0})
+                .rotation(if self.player_state == -1 { 135.0 * PI / 180.0 } else { if self.player_state == 1 { 225.0 * PI / 180.0 } else { 180.0 * PI / 180.0 }})
+                .offset(Point2 {x: 0.5, y: 0.5 });
+            draw(ctx, &self.player_image, player_draw_params)?;
+        }
 
         let opponent_draw_params = DrawParam::new()
             .dest(Point2 {x: self.opponent_x, y: 70.0})
@@ -85,15 +133,34 @@ impl EventHandler for GGEZ {
             quit(ctx);
         }
 
-        self.player_state = 0;
-        if is_key_pressed(ctx, KeyCode::Left) { self.player_state = -1 }
-        if is_key_pressed(ctx, KeyCode::Right) { self.player_state = 1 }
+        if keycode == KeyCode::Space && self.player_state != 2 {
+            self.player_grab_target_radian = (580.0_f64).atan2((self.player_x - self.player_target_x) as f64) as f32 + PI / 2.0;
+            
+            let grab_string_radian = self.player_grab_target_radian + 130.0 * PI / 180.0;
+            let grab_straigt_radian = self.player_grab_target_radian + 270.0 * PI / 180.0;
+            self.player_grab_string_position = Point2 {x: self.player_x + grab_string_radian.cos() * 80.0, y: 650.0 + grab_string_radian.sin() * 80.0};
+
+            self.player_grab_max_time = (650.0 + grab_string_radian.sin() * 80.0 - 170.0) / (grab_straigt_radian.sin() * self.grab_speed);
+            
+            self.player_grab_distance = (((self.player_target_x - self.player_x) * (self.player_target_x - self.player_x) + 480.0 * 480.0) as f64).sqrt() as f32;
+
+            self.player_grab_time = 0.0;
+            self.player_state = 2;
+        }
+
+        if self.player_state != 2 {
+            self.player_state = 0;
+            if is_key_pressed(ctx, KeyCode::Left) { self.player_state = -1 }
+            if is_key_pressed(ctx, KeyCode::Right) { self.player_state = 1 }
+        }
     }
 
     fn key_up_event(&mut self, ctx: &mut Context, keycode: KeyCode, keymod: KeyMods) {
-        self.player_state = 0;
-        if is_key_pressed(ctx, KeyCode::Left) { self.player_state = -1 }
-        if is_key_pressed(ctx, KeyCode::Right) { self.player_state = 1 }
+        if self.player_state != 2 {
+            self.player_state = 0;
+            if is_key_pressed(ctx, KeyCode::Left) { self.player_state = -1 }
+            if is_key_pressed(ctx, KeyCode::Right) { self.player_state = 1 }
+        }
     }
 }
 
@@ -118,6 +185,18 @@ fn main() {
         Ok(res) => res,
         Err(err) => panic!("Failed to load image: {}", err)
     };
+    let player_grab_image = match Image::new(&mut ctx, "/player_grab.png") {
+        Ok(res) => res,
+        Err(err) => panic!("Failed to load image: {}", err)
+    };
+    let grab_string_image = match Image::new(&mut ctx, "/grab_string.png") {
+        Ok(res) => res,
+        Err(err) => panic!("Failed to load image: {}", err)
+    };
+    let grab_hand_image = match Image::new(&mut ctx, "/grab_hand.png") {
+        Ok(res) => res,
+        Err(err) => panic!("Failed to load image: {}", err)
+    };
     let target_image = match Image::new(&mut ctx, "/target.png") {
         Ok(res) => res,
         Err(err) => panic!("Failed to load image: {}", err)
@@ -127,13 +206,22 @@ fn main() {
         background_image: background_image,
         foreground_image: foreground_image,
         player_image: player_image,
+        player_grab_image: player_grab_image,
+        grab_string_image: grab_string_image,
+        grab_hand_image: grab_hand_image,
         target_image: target_image,
         player_speed: 300.0,
         player_x: 640.0,
         player_target_x: 980.0,
         player_target_direction: -1,
+        player_grab_time: 0.0,
+        player_grab_max_time: 0.0,
+        player_grab_distance: 0.0,
+        player_grab_target_radian: 0.0,
+        player_grab_string_position: Point2 {x: 0.0, y: 0.0},
         opponent_x: 640.0,
         player_state: 0,
         target_speed: 800.0,
+        grab_speed: 600.0,
     });
 }
